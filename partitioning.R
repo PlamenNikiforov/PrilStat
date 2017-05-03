a52d059ca1411625365def39ad8482e79d9e165f
Hristiyan Markov <itzomarkov@gmail.com>
2017-05-03 15:27:00 +0300

    Update partitioning

--- partitioning.R
+++ partitioning.R
@@ -1,5 +1,7 @@
 # partition data in 2 sets
 
+library(caret)
+
 hrdata <- HR_comma_sep # save dataset copy
 
 trainIndex <- createDataPartition(hrdata$left, p = .7,list = FALSE,times = 1) # 70%-30% split
